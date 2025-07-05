ab9aa203d7a4c6b169340fd4b1716359bc4e45c5
Daniel Magalhães <danielmagalhaes38@hotmail.com>
2025-07-05 20:37:10 -0300

    g6.2

--- Scripts/Daniel/g6.1--g6.2--g6.3--g6.4.py
+++ Scripts/Daniel/g6.1--g6.2--g6.3--g6.4.py
@@ -55,7 +55,68 @@ except Exception as e:
 # PLANILHA
 # ************************
 
-# gráfico 6.1
+# # gráfico 6.1
+# try:
+#     data = c.open_file(dbs_path, 'ibge_conta_producao.zip', 'zip', excel_name='Tabela17', sheet_name='Tabela17.7', skiprows=1)
+#     indexes = data[data[data.columns[0]] == 'ANO'].index.tolist()  # extrai os índices das linhas que contêm os anos
+#     variables = data.iloc[[i - 3 for i in indexes], 0].to_list()  # extrai as variáveis correspondentes a cada ano
+#     # valores previsto: ['Valor Bruto da Produção 2010-2022', 'Consumo intermediário 2010-2022', 'Valor Adicionado Bruto 2010-2022']
+
+#     # tratamento dos dados
+#     dfs = []
+#     for i in range(len(indexes)):
+#         if i < 2:
+#             df = data.iloc[indexes[i]:indexes[i + 1]].copy()
+#         else:
+#             df = data.iloc[indexes[i]:].copy()
+
+#         columns = df.iloc[0].to_list()  # define a primeira linha como cabeçalho
+#         cols = [col.split('\n')[0].strip() if '\n' in col else col.strip() for col in columns]  # remove quebras de linha
+#         df.columns = cols  # renomeia as colunas
+#         # valores previstos: ['ANO', 'VALOR DO ANO ANTERIOR', 'ÍNDICE DE VOLUME', 'VALOR A PREÇOS DO ANO ANTERIOR', 'ÍNDICE DE PREÇO', 'VALOR A PREÇO CORRENTE']
+
+#         df[cols[0]] = df[cols[0]].astype(str)  # converte a primeira coluna para string
+#         df_filtered = df[
+#             (df[cols[0]].str.startswith('20')) & (df[cols[0]].str.len() == 4)
+#         ].copy()
+
+#         df_filtered[cols[0]] = df_filtered[cols[0]].astype(int)  # converte a primeira coluna para inteiro
+#         df_filtered[cols[1:]] = df_filtered[cols[1:]].astype(float)  # converte a segunda coluna para float
+#         df_filtered['Variável'] = variables[i]  # adiciona a variável correspondente
+
+#         # deflação
+#         df_filtered.sort_values(by=cols[0], ascending=False, inplace=True)  # ordena pelo ano
+#         df_filtered.reset_index(drop=True, inplace=True)
+#         df_filtered['Index'] = 100.00  # cria a coluna de índice
+        
+#         for row in range(1, len(df_filtered)):
+#             df_filtered.loc[row, 'Index'] = df_filtered.loc[row - 1, 'Index'] / df_filtered.loc[row -1, cols[-2]]
+
+#         dfs.append(df_filtered)
+
+#     df_concat = pd.concat(dfs, ignore_index=True)
+#     df_concat['Valor Ajustado'] = (df_concat[cols[-1]] / df_concat['Index']) * 100.00  # calcula o valor ajustado
+
+#     # organização da tabela
+#     df_concat = df_concat[[cols[0], 'Variável', 'Valor Ajustado']].copy()
+#     df_concat['Variável'] = df_concat['Variável'].apply(lambda x:
+#         'Valor bruto da produção' if 'valor bruto da produção' in x.lower().strip() else
+#         'Consumo intermediário' if 'consumo intermediário' in x.lower().strip() else
+#         'Valor bruto adicionado' if 'valor adicionado bruto' in x.lower().strip() else x
+#     )
+#     df_pivot = df_concat.pivot(index=cols[0], columns='Variável', values='Valor Ajustado').reset_index()
+#     df_pivot = df_pivot[[cols[0], 'Valor bruto da produção', 'Consumo intermediário', 'Valor bruto adicionado']].copy()
+
+#     df_pivot.rename(columns={'ANO': 'Ano'}, inplace=True)  # renomeia a coluna de ano
+#     df_pivot['Ano'] = df_pivot['Ano'].astype(int)  # converte a coluna de ano para inteiro
+
+#     df_pivot.to_excel(os.path.join(sheets_path, 'g6.1.xlsx'), index=False, sheet_name='g6.1')
+
+# except Exception as e:
+#     errors['Gráfico 6.1'] = traceback.format_exc()
+
+
+# gráfico 6.2
 try:
     data = c.open_file(dbs_path, 'ibge_conta_producao.zip', 'zip', excel_name='Tabela17', sheet_name='Tabela17.7', skiprows=1)
     indexes = data[data[data.columns[0]] == 'ANO'].index.tolist()  # extrai os índices das linhas que contêm os anos
@@ -94,26 +155,21 @@ try:
 
         dfs.append(df_filtered)
 
-    df_concat = pd.concat(dfs, ignore_index=True)
-    df_concat['Valor Ajustado'] = (df_concat[cols[-1]] / df_concat['Index']) * 100.00  # calcula o valor ajustado
+    df_concat = pd.concat(dfs, ignore_index=True).query('`Variável`.str.lower().str.contains("valor adicionado bruto")', engine='python')
+    df_concat['variação'] = (df_concat[cols[2]] - 1) * 100.00  # calcula o valor ajustado
 
     # organização da tabela
-    df_concat = df_concat[[cols[0], 'Variável', 'Valor Ajustado']].copy()
-    df_concat['Variável'] = df_concat['Variável'].apply(lambda x:
-        'Valor bruto da produção' if 'valor bruto da produção' in x.lower().strip() else
-        'Consumo intermediário' if 'consumo intermediário' in x.lower().strip() else
-        'Valor bruto adicionado' if 'valor adicionado bruto' in x.lower().strip() else x
-    )
-    df_pivot = df_concat.pivot(index=cols[0], columns='Variável', values='Valor Ajustado').reset_index()
-    df_pivot = df_pivot[[cols[0], 'Valor bruto da produção', 'Consumo intermediário', 'Valor bruto adicionado']].copy()
+    df_concat = df_concat[[cols[0], 'variação']].copy()
+    df_concat.dropna(axis=0, inplace=True)  # remove as linhas com valores nulos
 
-    df_pivot.rename(columns={'ANO': 'Ano'}, inplace=True)  # renomeia a coluna de ano
-    df_pivot['Ano'] = df_pivot['Ano'].astype(int)  # converte a coluna de ano para inteiro
+    df_concat.rename(columns={'ANO': 'Ano'}, inplace=True)  # renomeia a coluna de ano
+    df_concat['Ano'] = df_concat['Ano'].astype(int)  # converte a coluna de ano para inteiro
+    df_concat.sort_values(by='Ano', ascending=True, inplace=True)  # ordena pelo ano
 
-    df_pivot.to_excel(os.path.join(sheets_path, 'g6.1.xlsx'), index=False, sheet_name='g6.1')
+    df_concat.to_excel(os.path.join(sheets_path, 'g6.2.xlsx'), index=False, sheet_name='g6.2')
 
 except Exception as e:
-    errors['Gráfico 6.1'] = traceback.format_exc()
+    errors['Gráfico 6.2'] = traceback.format_exc()
 
 
 # # gráfico 5.2
@@ -265,7 +321,7 @@ except Exception as e:
 # se a chave do dicionário for url, o erro se refere à tentativa de download da base de dados
 # se a chave do dicionário for o nome da figura, o erro se refere à tentativa de estruturar a tabela
 if errors:
-    with open(os.path.join(errors_path, 'script--g5.1--g5.2--g5.3--g5.4.txt'), 'w', encoding='utf-8') as f:
+    with open(os.path.join(errors_path, 'script--g6.1--g6.2--g6.3--g6.4.txt'), 'w', encoding='utf-8') as f:
         f.write(json.dumps(errors, indent=4, ensure_ascii=False))
 
 # remove os arquivos baixados
