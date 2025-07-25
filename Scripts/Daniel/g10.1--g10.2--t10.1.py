3d2ba1eb0c56e977f13fa84dace2df6cd8aa2d1b
Daniel Magalhães <danielmagalhaes38@hotmail.com>
2025-07-25 00:43:00 -0300

    Finalizados scripts do grupo 10

--- Scripts/Daniel/g10.1--g10.2--t10.1.py
+++ Scripts/Daniel/g10.1--g10.2--t10.1.py
@@ -24,22 +24,22 @@ errors = {}
 # DOWNLOAD DA BASE DE DADOS
 # ************************
 
-# # sidra 5906
-# url = 'https://apisidra.ibge.gov.br/values/t/5906/n1/all/n3/all/v/7168/p/all/c11046/all/d/v7168%205?formato=json'
-# try:
-#     data = c.open_url(url)
-#     df = pd.DataFrame(data.json())
-#     df = df[['D3N', 'D1N', 'D4N', 'V']].copy()
-#     df.columns = ['Data', 'Região', 'Variável', 'Valor']
-#     df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
-#     df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
-#     df['Ano'] = df['Data'].str.split(' ').str[-1]  # extrai o ano da coluna Data
-#     df['Ano'] = df['Ano'].astype(int)  # converte a coluna Ano para inteiro
-#     df['Mês'] = df['Data'].str.split(' ').str[0]  # extrai o mês da coluna Data
+# sidra 5906
+url = 'https://apisidra.ibge.gov.br/values/t/5906/n1/all/n3/all/v/7168/p/all/c11046/all/d/v7168%205?formato=json'
+try:
+    data = c.open_url(url)
+    df = pd.DataFrame(data.json())
+    df = df[['D3N', 'D1N', 'D4N', 'V']].copy()
+    df.columns = ['Data', 'Região', 'Variável', 'Valor']
+    df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
+    df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
+    df['Ano'] = df['Data'].str.split(' ').str[-1]  # extrai o ano da coluna Data
+    df['Ano'] = df['Ano'].astype(int)  # converte a coluna Ano para inteiro
+    df['Mês'] = df['Data'].str.split(' ').str[0]  # extrai o mês da coluna Data
 
-#     c.to_excel(df, dbs_path, 'sidra_5906.xlsx')
-# except Exception as e:
-#     errors['Sidra 5906'] = traceback.format_exc()
+    c.to_excel(df, dbs_path, 'sidra_5906.xlsx')
+except Exception as e:
+    errors['Sidra 5906'] = traceback.format_exc()
 
 
 # sidra 2715 (IBGE - Pesquisa Anual de Serviços (PAS))
@@ -71,65 +71,57 @@ except Exception as e:
 # PLANILHA
 # ************************
 
-# # gráfico 10.1
-# try:
-#     data = c.open_file(dbs_path, 'sidra_5906.xlsx', 'xls', sheet_name='Sheet1').query(
-#         '`Mês` == "dezembro" and `Variável`.str.contains("receita nominal")', engine='python'
-#     )  # filtra os dados para o mês de dezembro e para as variáveis que contêm "receita nominal"
-#     data.sort_values(['Região', 'Variável', 'Ano'], inplace=True)  # ordena os dados por Região, Variável e Ano
+# gráfico 10.1
+try:
+    data = c.open_file(dbs_path, 'sidra_5906.xlsx', 'xls', sheet_name='Sheet1').query(
+        '`Mês` == "dezembro" and `Variável`.str.contains("receita nominal")', engine='python'
+    )  # filtra os dados para o mês de dezembro e para as variáveis que contêm "receita nominal"
+    data.sort_values(['Região', 'Variável', 'Ano'], inplace=True)  # ordena os dados por Região, Variável e Ano
     
-#     # tratamento dos dados principais
-#     df_ne = data.query('`Região` in @c.ne_states', engine='python').copy()
-#     assert df_ne['Região'].nunique() == 9, "Número de estados do NE diferente de 9"
-#     df_ne['Região'] = 'Nordeste'  # renomeia a região para Nordeste
-#     df_ne = df_ne.groupby(['Ano', 'Região', 'Variável'], as_index=False).agg({'Valor': 'mean'})  # agrupa por Ano e Variável, somando os valores
+    # tratamento dos dados principais
+    df_ne = data.query('`Região` in @c.ne_states', engine='python').copy()
+    assert df_ne['Região'].nunique() == 9, "Número de estados do NE diferente de 9"
+    df_ne['Região'] = 'Nordeste'  # renomeia a região para Nordeste
+    df_ne = df_ne.groupby(['Ano', 'Região', 'Variável'], as_index=False).agg({'Valor': 'mean'})  # agrupa por Ano e Variável, somando os valores
 
-#     df = data.loc[data['Região'].isin(['Brasil', 'Sergipe']), ['Ano', 'Região', 'Variável', 'Valor']].copy()  # seleciona as colunas relevantes
-#     df = pd.concat([df, df_ne], ignore_index=True)  # concatena os dados originais com os do Nordeste
-#     df['Variação'] = df.groupby(['Região', 'Variável'])['Valor'].pct_change() * 100  # calcula a variação percentual
+    df = data.loc[data['Região'].isin(['Brasil', 'Sergipe']), ['Ano', 'Região', 'Variável', 'Valor']].copy()  # seleciona as colunas relevantes
+    df = pd.concat([df, df_ne], ignore_index=True)  # concatena os dados originais com os do Nordeste
+    df['Variação'] = df.groupby(['Região', 'Variável'])['Valor'].pct_change() * 100  # calcula a variação percentual
 
-#     df_pivot = pd.pivot(df, index='Ano', columns='Região', values='Variação').reset_index()
-#     df_pivot.dropna(subset=['Brasil', 'Nordeste', 'Sergipe'], inplace=True)  # remove linhas onde Brasil ou Sergipe são NaN
+    df_pivot = pd.pivot(df, index='Ano', columns='Região', values='Variação').reset_index()
+    df_pivot.dropna(subset=['Brasil', 'Nordeste', 'Sergipe'], inplace=True)  # remove linhas onde Brasil ou Sergipe são NaN
 
-#     df_pivot.to_excel(os.path.join(sheets_path, 'g10.1.xlsx'), index=False, sheet_name='g10.1')
+    df_pivot.to_excel(os.path.join(sheets_path, 'g10.1.xlsx'), index=False, sheet_name='g10.1')
 
-# except Exception as e:
-#     errors['Gráfico 10.1'] = traceback.format_exc()
+except Exception as e:
+    errors['Gráfico 10.1'] = traceback.format_exc()
 
 
-# # gráfico 10.2
-# try:
-#     data = c.open_file(dbs_path, 'sidra_5906.xlsx', 'xls', sheet_name='Sheet1').query(
-#         '`Mês` == "dezembro" and `Variável`.str.contains("volume de serviços")', engine='python'
-#     )  # filtra os dados para o mês de dezembro e para as variáveis que contêm "receita nominal"
-#     data.sort_values(['Região', 'Variável', 'Ano'], inplace=True)  # ordena os dados por Região, Variável e Ano
+# gráfico 10.2
+try:
+    data = c.open_file(dbs_path, 'sidra_5906.xlsx', 'xls', sheet_name='Sheet1').query(
+        '`Mês` == "dezembro" and `Variável`.str.contains("volume de serviços")', engine='python'
+    )  # filtra os dados para o mês de dezembro e para as variáveis que contêm "receita nominal"
+    data.sort_values(['Região', 'Variável', 'Ano'], inplace=True)  # ordena os dados por Região, Variável e Ano
     
-#     # tratamento dos dados principais
-#     df_ne = data.query('`Região` in @c.ne_states', engine='python').copy()
-#     assert df_ne['Região'].nunique() == 9, "Número de estados do NE diferente de 9"
-#     df_ne['Região'] = 'Nordeste'  # renomeia a região para Nordeste
-#     df_ne = df_ne.groupby(['Ano', 'Região', 'Variável'], as_index=False).agg({'Valor': 'mean'})  # agrupa por Ano e Variável, somando os valores
+    # tratamento dos dados principais
+    df_ne = data.query('`Região` in @c.ne_states', engine='python').copy()
+    assert df_ne['Região'].nunique() == 9, "Número de estados do NE diferente de 9"
+    df_ne['Região'] = 'Nordeste'  # renomeia a região para Nordeste
+    df_ne = df_ne.groupby(['Ano', 'Região', 'Variável'], as_index=False).agg({'Valor': 'mean'})  # agrupa por Ano e Variável, somando os valores
 
-#     df = data.loc[data['Região'].isin(['Brasil', 'Sergipe']), ['Ano', 'Região', 'Variável', 'Valor']].copy()  # seleciona as colunas relevantes
-#     df = pd.concat([df, df_ne], ignore_index=True)  # concatena os dados originais com os do Nordeste
-#     df['Variação'] = df.groupby(['Região', 'Variável'])['Valor'].pct_change() * 100  # calcula a variação percentual
+    df = data.loc[data['Região'].isin(['Brasil', 'Sergipe']), ['Ano', 'Região', 'Variável', 'Valor']].copy()  # seleciona as colunas relevantes
+    df = pd.concat([df, df_ne], ignore_index=True)  # concatena os dados originais com os do Nordeste
+    df['Variação'] = df.groupby(['Região', 'Variável'])['Valor'].pct_change() * 100  # calcula a variação percentual
 
-#     df_pivot = pd.pivot(df, index='Ano', columns='Região', values='Variação').reset_index()
-#     df_pivot.dropna(subset=['Brasil', 'Nordeste', 'Sergipe'], inplace=True)  # remove linhas onde Brasil ou Sergipe são NaN
+    df_pivot = pd.pivot(df, index='Ano', columns='Região', values='Variação').reset_index()
+    df_pivot.dropna(subset=['Brasil', 'Nordeste', 'Sergipe'], inplace=True)  # remove linhas onde Brasil ou Sergipe são NaN
 
-#     df_pivot.to_excel(os.path.join(sheets_path, 'g10.2.xlsx'), index=False, sheet_name='g10.2')
+    df_pivot.to_excel(os.path.join(sheets_path, 'g10.2.xlsx'), index=False, sheet_name='g10.2')
 
-# except Exception as e:
-#     errors['Gráfico 10.2'] = traceback.format_exc()
+except Exception as e:
+    errors['Gráfico 10.2'] = traceback.format_exc()
 
-
-##########
-# A FONTE ESPECIFICADA NA DOCUMENTAÇÃO BAIXA ARQUIVOS ZIP, UM A UM PARA CADA ANO DA SÉRIE HISTÓRICA.
-# ISSO NÃO É PRÁTICO PARA O SCRIPT, ENTÃO FOI UTILIZADO O SIDRAPI PARA BAIXAR A BASE DE DADOS
-# CONTINUAR SCRIPT COM ESSA FONTE:
-# https://sidra.ibge.gov.br/tabela/2715
-# JÁ VERIFIQUEI E O SIDRAPI RETORNA OS MESMOS DADOS QUE A PESQUISA ANUAL DE SERVIÇOS (PAS) DO IBGE
-##########
 
 # tabela 10.1
 try:
