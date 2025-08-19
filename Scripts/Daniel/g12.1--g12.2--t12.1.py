782c585196c09c4f66cd3c7d613cff4d752fa771
Daniel Magalhães <danielmagalhaes38@hotmail.com>
2025-08-19 18:59:03 -0300

    para fechar o grupo 12, falta o script da figura t12.1; não entendi como baixar os dados

--- Scripts/Daniel/g12.1--g12.2--t12.1.py
+++ Scripts/Daniel/g12.1--g12.2--t12.1.py
@@ -98,117 +98,25 @@ except Exception as e:
     errors['Banco Central - BCB'] = traceback.format_exc()
 
 
-# import requests
-# import pandas as pd
-# from io import BytesIO
-# from datetime import datetime
-# import time
-
-# # Lista completa de séries
-# series_ids = [
-#     '13345', '13350', '13356', '13561', '13562', '13563',
-#     '13575', '13576', '13577', '13581', '13582', '13583',
-#     '13943', '13944', '13945', '20179', '20180', '20181'
-# ]
-
-# # Parâmetros fixos
-# start_date = '01/01/2010'
-# end_date = f'31/12/{datetime.now().year}'
-# max_series_per_request = 10
-
-# # URL base
-# consulta_url = "https://www3.bcb.gov.br/sgspub/consultarvalores/consultarValoresSeries.do?method=consultarValores"
-# download_url = "https://www3.bcb.gov.br/sgspub/consultarvalores/consultarValoresSeries.do?method=downLoad"
-
-# # Headers de navegador
-# headers = {
-#     'Content-Type': 'application/x-www-form-urlencoded',
-#     'Referer': 'https://www3.bcb.gov.br/sgspub/consultarvalores/telaCvsSelecionarSeries.paint',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
-# }
-
-# # DataFrame acumulador
-# df = pd.DataFrame()
-
-# # Loop em lotes de até 10 séries
-# for i in range(0, len(series_ids), max_series_per_request):
-#     batch = series_ids[i:i + max_series_per_request]
-#     batch_str = ';'.join(batch)
-
-#     form_data = {
-#         'optSelecionaSerie': batch,
-#         'dataInicio': start_date,
-#         'dataFim': end_date,
-#         'selTipoArqDownload': '0',
-#         'chkPaginar': 'on',
-#         'hdOidSeriesSelecionadas': batch_str,
-#         'hdPaginar': 'true',
-#         'bilServico': '[SGSFW2301]'
-#     }
-
-#     with requests.Session() as session:
-#         # Consulta
-#         session.post(consulta_url, headers=headers, data=form_data)
-        
-#         # Download do CSV
-#         download_headers = {
-#             'Referer': consulta_url,
-#             'User-Agent': headers['User-Agent'],
-#             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-#         }
-#         csv_response = session.get(download_url, headers=download_headers)
-
-#         # Leitura e concatenação
-#         df_batch = pd.read_csv(BytesIO(csv_response.content), sep=";", encoding="latin1")
-#         df = pd.concat([df, df_batch], ignore_index=True)
-
-#     # Pequeno intervalo entre requisições para evitar bloqueios
-#     time.sleep(1)
-
-# # Tratamento dos dados
-# df_final = df.loc[~(df[df.columns[0]] == 'Fonte')].copy()  # filtra apenas as linhas com datas válidas
-# df_melted = df_final.melt(id_vars=['Data'], value_vars=df_final.columns[1:], var_name='Variável', value_name='Valor')  # transforma os dados em formato longo
-# df_melted = df_melted.dropna(subset=['Valor'])  # remove linhas com valores NaN
-# df_melted['Região'] = df_melted['Variável'].str.split(' - ').str[2]  # extrai o estado da variável
-# df_melted['Variável'] = df_melted['Variável'].str.split(' - ').str[1]  # extrai a variável
-
-# df_export = df_melted[['Data', 'Variável', 'Região', 'Valor']].copy()  # seleciona as colunas relevantes
-# df_export['Data'] = pd.to_datetime(df_export['Data'], format='%m/%Y')  # converte a coluna de data para o formato datetime
-# df_export['Valor'] = df_export['Valor'].str.replace('.', '', regex=False).str.replace(',', '.', regex=False)  # ajusta o formato dos números
-# df_export['Valor'] = df_export['Valor'].astype(float)  # converte a coluna de valor para float
-
-# c.to_csv(df_export, dbs_path, 'bcb_series.csv')  # salva o DataFrame em CSV
-
-
-
-# # deflator IPEA IPCA
-# try:
-#     data = ipeadatapy.timeseries('PRECOS_IPCAG')
-#     data.rename(columns={'YEAR': 'Ano', 'VALUE ((% a.a.))': 'Valor'}, inplace=True)  # renomeia as colunas
-#     c.to_excel(data, dbs_path, 'ipeadata_ipca.xlsx')
-# except Exception as e:
-#     errors['IPEA IPCA'] = traceback.format_exc()
-
-
 # ************************
 # PLANILHA
 # ************************
 
-# # gráfico 12.1
-# try:
-#     # tratamento dos dados
-#     data = c.open_file(dbs_path, 'bcb.xlsx', 'xls', sheet_name='Sheet1').query('`Região` == "Sergipe"')  # filtra apenas as linhas com Sergipe
-#     data.loc[data['Variável'].str.startswith('Exportação'), 'Variável'] = 'Exportação'
-#     data.loc[data['Variável'].str.startswith('Importação'), 'Variável'] = 'Importação'
-#     data.loc[data['Variável'].str.startswith('Saldo Comercial'), 'Variável'] = 'Saldo Comercial'
-#     data['Data'] = data['Data'].dt.strftime('%d/%m/%Y')  # formata a data para ano-mês
+# gráfico 12.1
+try:
+    # tratamento dos dados
+    data = c.open_file(dbs_path, 'bcb.xlsx', 'xls', sheet_name='Sheet1').query('`Região` == "Sergipe"')  # filtra apenas as linhas com Sergipe
+    data.loc[data['Variável'].str.startswith('Exportação'), 'Variável'] = 'Exportação'
+    data.loc[data['Variável'].str.startswith('Importação'), 'Variável'] = 'Importação'
+    data.loc[data['Variável'].str.startswith('Saldo Comercial'), 'Variável'] = 'Saldo Comercial'
+    data['Data'] = data['Data'].dt.strftime('%d/%m/%Y')  # formata a data para ano-mês
 
-#     df_final = data[['Variável', 'Data', 'Valor']].copy()  # seleciona as colunas relevantes
+    df_final = data[['Variável', 'Data', 'Valor']].copy()  # seleciona as colunas relevantes
 
-#     df_final.to_excel(os.path.join(sheets_path, 'g12.1.xlsx'), index=False, sheet_name='g12.1')
+    df_final.to_excel(os.path.join(sheets_path, 'g12.1.xlsx'), index=False, sheet_name='g12.1')
 
-# except Exception as e:
-#     errors['Gráfico 12.1'] = traceback.format_exc()
+except Exception as e:
+    errors['Gráfico 12.1'] = traceback.format_exc()
 
 
 # gráfico 12.2
@@ -237,6 +145,17 @@ try:
 
 except Exception as e:
     errors['Gráfico 12.2'] = traceback.format_exc()
+
+
+# # tabela 12.1
+# try:
+#     # tratamento dos dados
+    
+
+#     df_final.to_excel(os.path.join(sheets_path, 't12.1.xlsx'), index=False, sheet_name='t12.1')
+
+# except Exception as e:
+#     errors['Tabela 12.2'] = traceback.format_exc()
 
 
 # geração do arquivo de erro caso ocorra algum
