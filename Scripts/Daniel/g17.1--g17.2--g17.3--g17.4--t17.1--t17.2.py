96e0f0735d11f9fb81e068596a19ed3475c7192e
Daniel Magalhães <danielmagalhaes38@hotmail.com>
2025-08-21 23:37:46 -0300

    t17.1

--- Scripts/Daniel/g17.1--g17.2--g17.3--g17.4--t17.1--t17.2.py
+++ Scripts/Daniel/g17.1--g17.2--g17.3--g17.4--t17.1--t17.2.py
@@ -71,36 +71,52 @@ errors = {}
 #     errors['Sidra 7113'] = traceback.format_exc()
 
 
-# sidra 356 - estimativa da população
-url = 'https://apisidra.ibge.gov.br/values/t/356/n1/all/n2/2/n3/28/v/1887/p/all/c1/6795/c2/6794/c58/2795/d/v1887%201?formato=json'
+# # sidra 356 - estimativa da população
+# url = 'https://apisidra.ibge.gov.br/values/t/356/n1/all/n2/2/n3/28/v/1887/p/all/c1/6795/c2/6794/c58/2795/d/v1887%201?formato=json'
+# try:
+#     data = c.open_url(url)
+#     df = pd.DataFrame(data.json())
+#     df = df[['D3N', 'D1N', 'V']].copy()
+#     df.columns = ['Ano', 'Região', 'Valor']
+#     df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
+#     df['Ano'] = df['Ano'].astype(int)
+#     df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
+
+#     c.to_excel(df, dbs_path, 'sidra_356.xlsx')
+# except Exception as e:
+#     errors['Sidra 356'] = traceback.format_exc()
+
+
+# # sidra 7126 - estimativa da população
+# url = 'https://apisidra.ibge.gov.br/values/t/7126/n1/all/n2/2/n3/28/v/3593/p/all/c2/6794/c58/2795/d/v3593%201?formato=json'
+# try:
+#     data = c.open_url(url)
+#     df = pd.DataFrame(data.json())
+#     df = df[['D3N', 'D1N', 'V']].copy()
+#     df.columns = ['Ano', 'Região', 'Valor']
+#     df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
+#     df['Ano'] = df['Ano'].astype(int)
+#     df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
+
+#     c.to_excel(df, dbs_path, 'sidra_7126.xlsx')
+# except Exception as e:
+#     errors['Sidra 7126'] = traceback.format_exc()
+
+
+# sidra 7143 - estimativa da população
+url = 'https://apisidra.ibge.gov.br/values/t/7143/n1/all/n2/2/n3/28/v/10274/p/all/c872/47821,47823,47825,47826/c11797/allxt/d/v10274%201?formato=json'
 try:
     data = c.open_url(url)
     df = pd.DataFrame(data.json())
-    df = df[['D3N', 'D1N', 'V']].copy()
-    df.columns = ['Ano', 'Região', 'Valor']
+    df = df[['D3N', 'D1N', 'D5N', 'D4N', 'V']].copy()
+    df.columns = ['Ano', 'Região', 'Rede', 'Nível', 'Valor']
     df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
     df['Ano'] = df['Ano'].astype(int)
     df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
 
-    c.to_excel(df, dbs_path, 'sidra_356.xlsx')
+    c.to_excel(df, dbs_path, 'sidra_7143.xlsx')
 except Exception as e:
-    errors['Sidra 356'] = traceback.format_exc()
-
-
-# sidra 7126 - estimativa da população
-url = 'https://apisidra.ibge.gov.br/values/t/7126/n1/all/n2/2/n3/28/v/3593/p/all/c2/6794/c58/2795/d/v3593%201?formato=json'
-try:
-    data = c.open_url(url)
-    df = pd.DataFrame(data.json())
-    df = df[['D3N', 'D1N', 'V']].copy()
-    df.columns = ['Ano', 'Região', 'Valor']
-    df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
-    df['Ano'] = df['Ano'].astype(int)
-    df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
-
-    c.to_excel(df, dbs_path, 'sidra_7126.xlsx')
-except Exception as e:
-    errors['Sidra 7126'] = traceback.format_exc()
+    errors['Sidra 7143'] = traceback.format_exc()
 
 
 # ************************
@@ -128,23 +144,40 @@ except Exception as e:
 #     errors['Gráfico 17.1'] = traceback.format_exc()
 
 
-# gráfico 17.2
+# # gráfico 17.2
+# try:
+#     # tabela ibge
+#     data = c.open_file(dbs_path, 'sidra_356.xlsx', 'xls', sheet_name='Sheet1')
+#     data_new = c.open_file(dbs_path, 'sidra_7126.xlsx', 'xls', sheet_name='Sheet1')
+    
+#     df = pd.concat([data, data_new], ignore_index=True)
+
+#     df['Ano'] = '01/01/' + df['Ano'].astype(str)  # formata a coluna Ano para o formato de data
+#     df.sort_values(by=['Região', 'Ano'], inplace=True)  # ordena os dados por Região e Ano
+
+#     df_final = df[['Região', 'Ano', 'Valor']].copy()
+
+#     c.to_excel(df_final, sheets_path, 'g17.2.xlsx')
+
+# except Exception as e:
+#     errors['Gráfico 17.2'] = traceback.format_exc()
+
+
+# tabela 17.1
 try:
     # tabela ibge
-    data = c.open_file(dbs_path, 'sidra_356.xlsx', 'xls', sheet_name='Sheet1')
-    data_new = c.open_file(dbs_path, 'sidra_7126.xlsx', 'xls', sheet_name='Sheet1')
-    
-    df = pd.concat([data, data_new], ignore_index=True)
+    data = c.open_file(dbs_path, 'sidra_7143.xlsx', 'xls', sheet_name='Sheet1')
 
-    df['Ano'] = '01/01/' + df['Ano'].astype(str)  # formata a coluna Ano para o formato de data
-    df.sort_values(by=['Região', 'Ano'], inplace=True)  # ordena os dados por Região e Ano
+    data['Ano'] = '01/01/' + data['Ano'].astype(str)  # formata a coluna Ano para o formato de data
+    data.sort_values(by=['Região', 'Ano', 'Nível', 'Rede'], inplace=True)  # ordena os dados por Região e Ano
+    data.rename(columns={'Rede': 'Rede de ensino', 'Nível': 'Curso frequentado'}, inplace=True)
 
-    df_final = df[['Região', 'Ano', 'Valor']].copy()
+    df_final = data[['Região', 'Curso frequentado', 'Rede de ensino', 'Ano', 'Valor']].copy()
 
-    c.to_excel(df_final, sheets_path, 'g17.2.xlsx')
+    c.to_excel(df_final, sheets_path, 't17.1.xlsx')
 
 except Exception as e:
-    errors['Gráfico 17.2'] = traceback.format_exc()
+    errors['Tabela 17.1'] = traceback.format_exc()
 
 
 # geração do arquivo de erro caso ocorra algum
