3da1a287d6b940c5d7a939b4c403e4defcedbf5e
Daniel Magalhães <danielmagalhaes38@hotmail.com>
2025-08-22 00:28:42 -0300

    iniciada g17.3

--- Scripts/Daniel/g17.1--g17.2--g17.3--g17.4--t17.1--t17.2.py
+++ Scripts/Daniel/g17.1--g17.2--g17.3--g17.4--t17.1--t17.2.py
@@ -103,20 +103,40 @@ errors = {}
 #     errors['Sidra 7126'] = traceback.format_exc()
 
 
-# sidra 7143 - estimativa da população
-url = 'https://apisidra.ibge.gov.br/values/t/7143/n1/all/n2/2/n3/28/v/10274/p/all/c872/47821,47823,47825,47826/c11797/allxt/d/v10274%201?formato=json'
-try:
-    data = c.open_url(url)
-    df = pd.DataFrame(data.json())
-    df = df[['D3N', 'D1N', 'D5N', 'D4N', 'V']].copy()
-    df.columns = ['Ano', 'Região', 'Rede', 'Nível', 'Valor']
-    df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
-    df['Ano'] = df['Ano'].astype(int)
-    df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
+# # sidra 7143 - estimativa da população
+# url = 'https://apisidra.ibge.gov.br/values/t/7143/n1/all/n2/2/n3/28/v/10274/p/all/c872/47821,47823,47825,47826/c11797/allxt/d/v10274%201?formato=json'
+# try:
+#     data = c.open_url(url)
+#     df = pd.DataFrame(data.json())
+#     df = df[['D3N', 'D1N', 'D5N', 'D4N', 'V']].copy()
+#     df.columns = ['Ano', 'Região', 'Rede', 'Nível', 'Valor']
+#     df.drop(0, axis='index', inplace=True)  # remove a primeira linha que contém o cabeçalho
+#     df['Ano'] = df['Ano'].astype(int)
+#     df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')  # converte a coluna Valor para numérico, tratando erros
 
-    c.to_excel(df, dbs_path, 'sidra_7143.xlsx')
+#     c.to_excel(df, dbs_path, 'sidra_7143.xlsx')
+# except Exception as e:
+#     errors['Sidra 7143'] = traceback.format_exc()
+
+
+# ideb resultados anos iniciais
+url = 'https://www.gov.br/inep/pt-br/areas-de-atuacao/pesquisas-estatisticas-e-indicadores/ideb/resultados'
+try:
+    driver = c.Google(visible=True, rep=dbs_path)  # instância do objeto driver do Selenium
+    driver.get(url)  # acessa a página
+    time.sleep(2)
+    driver.random_click()
+    driver.wait('/html/body/div[2]/div[1]/main/div[2]/div/div[4]/div[2]/div/div/ul[4]/li/a')
+    driver.click('/html/body/div[2]/div[1]/main/div[2]/div/div[4]/div[2]/div/div/ul[4]/li/a')
+    time.sleep(2)
+
+    data = c.open_file(dbs_path, os.listdir(dbs_path)[0], ext='zip', excel_name='divulgacao', skiprows=9)
+    df = data[list(data.keys())[0]].copy()  # cópia da primeira aba da planilha
+
+    # INICAR TRATAMENTO DOS DADOS
 except Exception as e:
-    errors['Sidra 7143'] = traceback.format_exc()
+    errors['IDEB Resultados'] = traceback.format_exc()
+
 
 
 # ************************
