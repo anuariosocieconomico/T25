ddd66d6ca98639498508524d924da8bd8a2bfcba
Daniel Magalhães <danielmagalhaes38@hotmail.com>
2025-09-29 21:30:56 -0300

    criada tabela final da pam para consumo no BI

--- Scripts/Municipios/mart_pam_consolidado.py
+++ Scripts/Municipios/mart_pam_consolidado.py
@@ -18,6 +18,7 @@ os.makedirs(mart_path, exist_ok=True)
 # arquivos de origem
 temporaria_path = os.path.join(raw_path, 'producao_agricola_municipal_temporaria', 'raw_producao_agricola_municipal_temporaria_consolidado.parquet')
 permanente_path = os.path.join(raw_path, 'producao_agricola_municipal_permanente', 'raw_producao_agricola_municipal_permanente_consolidado.parquet')
+ipca_path = os.path.join(raw_path, 'ipca.parquet')
 
 # arquivo de destino
 consolidated_path = os.path.join(mart_path, 'producao_agricola_municipal_consolidado.parquet')
@@ -45,12 +46,54 @@ if os.path.exists(permanente_path):
 else:
     print('ERRO: Base PAM permanente não encontrada')
 
+if os.path.exists(ipca_path):
+    df_ipca = pd.read_parquet(ipca_path)
+    df_ipca_annual = df_ipca.loc[df_ipca['Mes'].dt.month == 12].copy()  # filtra apenas dezembro de cada ano
+    df_ipca_annual.rename(columns={'Mes': 'Ano'}, inplace=True)  # renomeia para Ano, para facilitar o merge
+    
+    print(f'IPCA: {len(df_ipca)} registros')
+else:
+    print('ERRO: Base IPCA não encontrada')
+
 # consolida as bases
 if data_frames:
     print('Consolidando...')
-    df_final = pd.concat(data_frames, ignore_index=True)
-    
-    # salva o arquivo consolidado
+    df_concat = pd.concat(data_frames, ignore_index=True).query('not `Variável`.str.contains("percentual do total")', engine='python')  # filtra apenas as variáveis de produção
+    df_concat['Município'] = df_concat['Município'].str.replace(' (SE)', '', regex=False)  # remove a sigla do estado
+    df_concat['Ano'] = pd.to_datetime('01/12/' + df_concat['Ano'].astype(str), format='%d/%m/%Y', errors='coerce')  # padroniza a data para 1º de dezembro do ano, para facilitar o merge com o IPCA
+
+    # pivotiza as variáveis em colunas para calcular o ranking e inflação
+    variables = df_concat['Variável'].unique()
+    rows = [col for col in df_concat.columns if col != 'Valor' and col != 'Variável']
+    df_pivoted = df_concat.pivot(index=rows, columns='Variável', values='Valor').reset_index()
+
+    # trata a inflação
+    df_merged = pd.merge(df_pivoted, df_ipca_annual[['Ano', 'indice_ipca']], how='left', on='Ano', validate='m:1')
+    ano_mais_recente = df_merged.loc[df_merged['indice_ipca'].notnull(), 'Ano'].max()  # ano mais recente com índice disponível
+    max_ipca = df_merged.loc[df_merged['Ano'] == ano_mais_recente, 'indice_ipca'].max()  # índice do ano mais recente
+    df_merged['Inflação'] = max_ipca / df_merged['indice_ipca']  # fator de correção para valores reais
+    df_merged['Valor real da produção'] = df_merged['Valor da produção'] * df_merged['Inflação']  # ajusta o valor pela inflação
+    df_merged.rename(columns={'Valor da produção': 'Valor nominal da produção'}, inplace=True)  # renomeia a coluna original
+
+    # calcula o ranking dentro do ano
+    # desconsidera o valor da produção, adicionando à lista o valor real e nominal
+    for col in [v for v in variables if v != 'Valor da produção'] + ['Valor real da produção', 'Valor nominal da produção']:
+        df_merged[f'Ranking {col}'] = df_merged.groupby(['Tipo_Lavoura', 'Produto', 'Ano'])[col].rank(method='min', ascending=False)
+
+    # coluna adicional e ranking
+    df_merged['Valor médio real unitário'] = df_merged['Valor real da produção'] / df_merged['Quantidade produzida']
+    df_merged['Ranking Valor médio real unitário'] = df_merged.groupby(['Tipo_Lavoura', 'Produto', 'Ano'])['Valor médio real unitário'].rank(method='min', ascending=False)
+
+    # ajustes finais
+    pivoted_columns = [v for v in variables if v != 'Valor da produção'] + ['Valor nominal da produção', 'Valor real da produção', 'Valor médio real unitário']
+    ranked_columns = [f'Ranking {v}' for v in variables if v != 'Valor da produção'] + ['Ranking Valor nominal da produção', 'Ranking Valor real da produção', 'Ranking Valor médio real unitário']
+    df_final = df_merged[['Ano', 'Município', 'Tipo_Lavoura', 'Unidade de Medida', 'Produto'] + pivoted_columns + ranked_columns].copy()
+    df_final.rename(columns={'Tipo_Lavoura': 'Lavoura'}, inplace=True)  # padroniza o nome da coluna
+    max_year = df_final['Ano'].dt.year.max()
+    df_final = df_final.query('Ano.dt.year >= @max_year - 4', engine='python')  # filtra a partir de 2004
+    df_final['Ano'] = '31/12/' + df_final['Ano'].dt.year.astype(str)  # padroniza a data para o final do ano
+
+    # salva o arquivo consolidado e tratado
     df_final.to_parquet(consolidated_path, engine='pyarrow', compression='snappy', index=False)
     print(f'Consolidado salvo: {len(df_final)} registros totais')
     print(f'Arquivo: {consolidated_path}')
