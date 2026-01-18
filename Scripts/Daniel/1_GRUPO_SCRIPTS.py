9daa507fa52c95bb700310a3159d80b130c04051
Daniel Magalhães <danielmagalhaes38@hotmail.com>
2026-01-18 19:54:54 -0300

    atualização do yml para tentar resolver problema de commit, que não envia as atualizações + comentário no script de execeução para diminuir o tempo de execução durante os testes. restaurar o arquivo após finalização dos testes

--- Scripts/Daniel/1_GRUPO_SCRIPTS.py
+++ Scripts/Daniel/1_GRUPO_SCRIPTS.py
@@ -34,10 +34,15 @@ current_month = datetime.datetime.now().month
 # Obtém o diretório atual onde o script principal está localizado
 script_path = os.path.dirname(os.path.abspath(__file__))
 
+# scripts_to_run = [
+#     'g1.5--g1.6--g1.7--g1.8--t1.1.py', 'g10.1--g10.2--t10.1.py', 'g11.1--ate--g11.8--t11.1--t11.2.py', 'g12.1--g12.2--t12.1.py', 'g13.1a--ate-t13.3.py',
+#     'g13.6.py', 'g14.1--g14.2--t14.1--t14.2.py', 'g15.1--g15.2--g15.3--g15.4.py', 'g16.1--t16.1--g16.3--g16.4--t16.2.py',
+#     'g17.1--g17.2--g17.3--g17.4--t17.1--t17.2.py', 'g18.1--g18.2.py', 'g18.3.py'
+# ]
+
+# COMENTADOS SCRIPTS APRA TESTES NO RUNNER, REMOVER APÓS FINALIZAÇÃO DOS TESTES
 scripts_to_run = [
-    'g1.5--g1.6--g1.7--g1.8--t1.1.py', 'g10.1--g10.2--t10.1.py', 'g11.1--ate--g11.8--t11.1--t11.2.py', 'g12.1--g12.2--t12.1.py', 'g13.1a--ate-t13.3.py',
-    'g13.6.py', 'g14.1--g14.2--t14.1--t14.2.py', 'g15.1--g15.2--g15.3--g15.4.py', 'g16.1--t16.1--g16.3--g16.4--t16.2.py',
-    'g17.1--g17.2--g17.3--g17.4--t17.1--t17.2.py', 'g18.1--g18.2.py', 'g18.3.py'
+    'g1.5--g1.6--g1.7--g1.8--t1.1.py', 'g10.1--g10.2--t10.1.py'
 ]
 
 # # TODOS OS SCRIPTS PARA TESTAR ADAPTABILDIADE NO RUNNER DO GITHUB
